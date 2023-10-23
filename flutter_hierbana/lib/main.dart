51d71612efb3c54417964e122d929ecda7d8a4c4
AlexEFpro <alejandro.salinas@udgvirtual.udg.mx>
2023-10-23 09:14:46 -0600

    Inicio del repositorio base del proyecto

---
+++ flutter_hierbana/lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hierbana/screens/catalogo_screen.dart';
+
+  
+
+void main() {
+
+runApp(const MyApp());
+
+}
+
+  
+
+class MyApp extends StatelessWidget {
+
+const MyApp({super.key});
+
+  
+
+// This widget is the root of your application.
+
+@override
+
+Widget build(BuildContext context) {
+
+return  MaterialApp(
+theme: ThemeData(
+  useMaterial3: true,
+  colorSchemeSeed: Colors.greenAccent
+   ),
+debugShowCheckedModeBanner:false,
+
+home: const CatalogoScreen()
+);
+
+}
+
+}
